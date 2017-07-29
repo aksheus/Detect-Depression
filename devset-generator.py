cac1cbaf3e376947faf46ab02416bd98aebcb576
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-07-29 15:20:26 -0500

    added get usernames function

--- devset-generator.py
+++ devset-generator.py
@@ -10,6 +10,25 @@ import argparse
    		4) maintain ratio of positive - negative users
 
 """
+join = lambda x,y: os.path.join(x,y)
+isdir = lambda x: os.path.isdir(x)
+isfile = lambda y: os.path.isfile(y)
+
+
+def get_usernames(path):
+	# get user names from <path>/<positive folder>/<chunk1-2>
+
+	subdirs = [ join(path,subdir) for subdir in os.listdir(path) if isdir(join(path,subdir))]
+	print 
+	positive_dir = [ subdir for subdir in subdirs if 'positive' in subdir ] [0]
+	target_folder = positive_dir+'/'+'chunk1-2'
+
+	if not isdir(target_folder):
+		print 'positive users subfolder does not have chunk1-2 !!'
+		exit(1)
+
+	return [ u.split('.')[0] for u in os.listdir(target_folder) if isfile(join(target_folder,u)) ]
+
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='USAGE :   python deveset_generator.py --path . --oversampling yes --fparam 4 ')
@@ -17,5 +36,10 @@ if __name__ == '__main__':
 	parser.add_argument('-o','--oversampling',help='if oversampling say yes else no',required=True)
 	parser.add_argument('-f','--fparam',help='f parameter')
 	args= vars(parser.parse_args())
+
+	print get_usernames(args['path'])
+
+
+
 
 	
